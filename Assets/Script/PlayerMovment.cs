5e349067015126e3c368d684e9bfd1bc94ebac7d
NatasaLuki <163592601+NatasaLuki@users.noreply.github.com>
2024-03-15 21:36:41 +0100

    Created project YAYY

---
+++ Assets/Script/PlayerMovment.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    string helloWorld = "Hello World";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(helloWorld);
+    }
+}
